9d152af067be7951ade857410ea6ea4abc08626a
Domenico Nappo <domenico.nappo@gmail.com>
2020-04-24 20:45:34 +0200

    generate update pdf

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+# Dependencies:
+# pandoc > 2.0
+#  - On Ubuntu 18.04, install latest pandoc deb package from https://github.com/jgm/pandoc/releases
+# texlive and xelatex pdf engine
+#  - Install texlive packages: sudo apt-get -y install texlive-xetex texlive-latex-recommended texlive-pictures texlive-latex-extra
+# gs script
+
 yaml_header="---
   # ...
   header-includes:
@@ -28,10 +36,10 @@ do
   fi
 done
 
-echo "pandoc index_pdf.md --normalize --smart --template=template.latex -o _pdf/frontpage.pdf -V geometry:margin=1.5cm"
+echo "pandoc index_pdf.md --template=template.latex -f markdown+smart -o _pdf/frontpage.pdf -V geometry:margin=1.5cm"
 pandoc index_pdf.md --template=template.latex -f markdown+smart -o _pdf/frontpage.pdf -V geometry:margin=1.5cm
 
-echo "pandoc _pdf/*.md  --toc -V toc-title:\"Table of Contents\" --template=template.latex -f markdown+smart --pdf-engine=xelatex -o _pdf/manual.pdf -V geometry:margin=1.5cm"
+echo "pandoc _pdf/*.md --toc -V toc-title:"Table of Contents" --template=template.latex --read=markdown+smart --write=pdf --pdf-engine=xelatex -o _pdf/manual.pdf -V geometry:margin=1.5cm"
 pandoc _pdf/*.md --toc -V toc-title:"Table of Contents" --template=template.latex --read=markdown+smart --write=pdf --pdf-engine=xelatex -o _pdf/manual.pdf -V geometry:margin=1.5cm
 
 echo "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dAutoRotatePages=/None -sOutputFile=./_pdf/Lisflood_Model.pdf ./_pdf/frontpage.pdf ./_pdf/manual.pdf"
--- index_pdf.md
+++ index_pdf.md
@@ -3,7 +3,7 @@
 
 # LISFLOOD - a distributed hydrological rainfall-runoff model
 
-## Model Documentation
+## Model Documentation (last update 21 Feb 2020)
 
 ![](media/image6-frontpage.png)
 
